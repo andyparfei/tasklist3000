9f8db7ed85285d9e74931e71804ab0590bfba606
Andy Parfei <andy@andyparfei.dev>
2025-02-27 11:50:34 +0200

    Use persistent storage (docker volumes) for storing the database file

--- backend/src/tasklist3000/config.py
+++ backend/src/tasklist3000/config.py
@@ -2,7 +2,8 @@ import os
 
 BACKEND_FRAMEWORK = "robyn" # "fastapi" or "flask" or "robyn"
 
-DB_PATH = os.getenv("DATABASE_URL", "sqlite:///./tasks.db")
+# For Docker, use a path in /data which will be mounted as a volume
+DB_PATH = os.getenv("DATABASE_URL", "sqlite:////data/tasks.db")
 PORT="8080"
 HOST="0.0.0.0" # Listen on all interfaces
 CORS_ALLOWED_ORIGINS = ["http://localhost:5173"]
